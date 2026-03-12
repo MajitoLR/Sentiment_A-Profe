14756cff5239b6a69c5e51cfc2e8266e22f4b019
MajitoLR <larreamariajose456@gmail.com>
2026-03-12 13:32:03 -0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from textblob import TextBlob
+from stramlit_lottie import st_lottie
+import json
 import pandas as pd
 import streamlit as st
 from PIL import Image
@@ -35,7 +37,16 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
+            with open ('Dancing_dog2x20.json') as source:
+              animation=json.load(source)
+            st.lottie(animation,width =350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
+            with open ('Boxing_with_Bone-Angry Puppy.json') as source:
+              animation=json.load(source)
+            st.lottie(animation,width =350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+            with open ('Cute_Doggie.json') as source:
+              animation=json.load(source)
+            st.lottie(animation,width =350)
